6c3a87afc54750e04383b25d688070b40d341b0f
sharkbyte <sgquin@gmail.com>
2022-05-10 10:47:17 -0700

    fix typo

--- controllers/api/questMasterRoutes.js
+++ controllers/api/questMasterRoutes.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
-const { Adventurer, Quest } = require("../../models");
+const { Questmaster, Adventurer, Quest } = require("../../models");
 
+router.post("/", async (req, res) => {
+  try {
+    const user = await Questmaster.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+      res
+        .status(200)
+        .json({ user: user, message: `New user ${req.body.name} created!` });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  res.status(200);
+});
 module.exports = router;
--- controllers/api/questRoutes.js
+++ controllers/api/questRoutes.js
@@ -1,5 +1,20 @@
 const router = require("express").Router();
 const { Adventurer, Quest } = require("../../models");
 
-// router.post("/", async);
+router.post("/", async (req, res) => {
+  try {
+    const user = await Quest.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+      res
+        .status(200)
+        .json({ user: user, message: `New user ${req.body.name} created!` });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+  res.status(200);
+});
+
 module.exports = router;
