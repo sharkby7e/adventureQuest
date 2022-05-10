8ee4099ba64c141cbcdb247c53a059c1f738015b
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-05-10 13:43:29 -0700

    progress on game mechanics

--- public/js/character.js
+++ public/js/character.js
@@ -3,3 +3,52 @@ function displayPointsLeft() {
   var pointsLeft = 15 - strChoice + dexChoice + intChoice;
   $('#remaining-points').text(pointsLeft);
   window.setTimeout("displayPointsLeft()", 10);
+}
+
+
+
+
+
+function attackProbability() {
+  var adventurerStr;
+  var adventurerDex;
+  var adventurerInt;
+
+}
+
+var strMultplier;
+var dexMultplier;
+var intMultplier;
+
+
+
+
+async function init() {
+  classData = await fetch('/api/class/')
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      return data;
+    });
+  adventurerData = await fetch('/api/class/')
+  .then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    return data;
+  });
+
+
+  adventurerData[classChoice].strMultiplier;
+
+
+      adventurerStr * classData[classChoice].strMultiplier;
+      adventurerDex * classData[classChoice].dexMultiplier;
+      adventurerInt * classData[classChoice].intMultiplier;
+
+
+
+
+}
+
+
+init();
