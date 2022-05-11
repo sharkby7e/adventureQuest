57926ef6d6bb775efe29146b3469d0213d077f0d
sharkbyte <sgquin@gmail.com>
2022-05-11 14:50:15 -0700

    attempting to fix route bugs

--- controllers/api/questRoutes.js
+++ controllers/api/questRoutes.js
@@ -1,20 +1,25 @@
 const router = require("express").Router();
 const { Adventurer, Quest } = require("../../models");
 
-router.post("/", async (req, res) => {
-  try {
-    const user = await Quest.create(req.body);
-    req.session.save(() => {
-      req.session.user_id = user.id;
-      req.session.logged_in = true;
-      res
-        .status(200)
-        .json({ user: user, message: `New user ${req.body.name} created!` });
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-  res.status(200);
+// router.post("/", async (req, res) => {
+//   try {
+//     const user = await Quest.create(req.body);
+//     req.session.save(() => {
+//       req.session.user_id = user.id;
+//       req.session.logged_in = true;
+//       res
+//         .status(200)
+//         .json({ user: user, message: `New user ${req.body.name} created!` });
+//     });
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+//   res.status(200);
+// });
+
+router.post("/random", async (req, res) => {
+  // res.json("hello");
+  res.json(req.body);
 });
 
 module.exports = router;
