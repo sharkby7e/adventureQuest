52b2e15006b82cb02f072e140f120abd3f3ad3e6
Krystopher Quintero <krystopher.q@gmail.com>
2022-05-11 19:42:56 -0700

    minor changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,7 +8,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   try {
-    res.render("landing");
+    res.render("landing",{logged_in: req.session.logged_in});
   } catch (err) {
     res.status(500).json(err);
   }
